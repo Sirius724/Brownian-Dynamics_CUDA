5b716dc299263635c45d4391e4a1a2e30d886808
JEONG KANGEUN <44566223+Sirius724@users.noreply.github.com>
2023-12-08 16:59:47 +0900

    Update compile_mpi.sh

--- MPI/compile_mpi.sh
+++ MPI/compile_mpi.sh
@@ -1,1 +1,1 @@
-nvcc measure_BHHP.cu -o measure_BHHP.out -I/home/appl/openmpi-1.6.4-ic1217/include -L/home/appl/openmpi-1.6.4-ic1217/lib -I ~/inlcude -L ~/lib -lsprng -lcudart -lmpi
+nvcc measure_BHHP_mpi.cu -o measure_BHHP.out -I/home/appl/openmpi-1.6.4-ic1217/include -L/home/appl/openmpi-1.6.4-ic1217/lib -I ~/inlcude -L ~/lib -lsprng -lcudart -lmpi
